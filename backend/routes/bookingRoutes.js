179b44df1f274aa03824784a7d65662a0a892e32
Juan Lanuza <jgablanuza@gmail.com>
2026-05-02 11:43:02 +0800

    routing fix

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -12,26 +12,11 @@ import {
 
 const router = express.Router();
 
-// ==========================================
-// 🧑‍💻 PUBLIC / USER ROUTES
-// ==========================================
 router.post("/create-booking", requireUser, createBooking);
 router.get("/my-bookings", requireUser, getUserBookings);
-router.get("/by-reference/:reference", requireUser, getBookingByReference);
-router.get("/bookings-total-month", requireUser, getBookingsTotalBaseOnMonth);
-router.post("/cancel/:id", requireUser, cancelBooking);
 router.post("/verify-payment", requireUser, verifyTokenCheckout);
-
-// ==========================================
-// 🛡️ ADMIN / EMPLOYEE ROUTES
-// ==========================================
 router.get("/all-bookings", requireUser, requireAdmin, getAllBookings);
-router.put("/:id", requireUser, requireAdmin, updateBooking);
-router.delete("/:id", requireUser, requireAdmin, deleteBooking);
 
-// Cancellation Management
-router.get("/cancellations", requireUser, requireAdmin, getcancellations);
-router.post("/cancellations/:id/approve", requireUser, requireAdmin, approveCancellation);
-router.post("/cancellations/:id/reject", requireUser, requireAdmin, disApproveCancellation);
+
 
 export default router;
