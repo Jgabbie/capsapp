fc8467acca2e2dbceefdfe47edb8b7cdc2ce6fef
Marion Balmonte <mflameion@gmail.com>
2026-05-03 19:22:11 +0800

    cancel

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -5,6 +5,7 @@ import requireAdmin from "../middleware/requireAdmin.js";
 // 🔥 Make sure ALL of these are exported in your mobile bookingController.js!
 import {
   createBooking,
+  cancelBooking,
   getUserBookings,
   getAllBookings,
   verifyTokenCheckout,
@@ -16,6 +17,7 @@ const router = express.Router();
 
 router.post("/create-booking", requireUser, createBooking);
 router.get("/my-bookings", requireUser, getUserBookings);
+router.post("/cancel/:id", requireUser, cancelBooking);
 router.get('/bookings-total-month', requireUser, getBookingsTotalBaseOnMonth);
 router.get('/invoice-number/:reference', requireUser, getInvoiceNumber);
 router.post("/verify-payment", requireUser, verifyTokenCheckout);
