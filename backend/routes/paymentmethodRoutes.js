6112466f63ae6a174a38c04c40df604dc57f50ae
Juan Lanuza <jgablanuza@gmail.com>
2026-07-19 16:34:45 +0800

    minor changes

--- backend/routes/paymentmethodRoutes.js
+++ backend/routes/paymentmethodRoutes.js
@@ -8,6 +8,7 @@ const upload = multer({
     dest: "uploads/payment-methods",
 });
 
+// Payment Method Routes
 router.get("/get-methods", paymentmethodController.getMethods);
 router.post("/create-methods", upload.single("image"), paymentmethodController.createMethod);
 router.put("/:id/update-methods", upload.single("image"), paymentmethodController.updateMethod);
